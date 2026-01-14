4ef2fb6acd872bfadf5333e7ba55c8183da2d2b9
Isaac Lafond <isaaclafond@Isaacs-MacBook-Pro.local>
2026-01-14 17:28:44 -0500

    Refactor UI structure: replace home module with home UI function, update UMAP module, and add coming soon feature with new assets.

--- app/app.R
+++ app/app.R
@@ -1,69 +1,73 @@
-
 library(shiny)
 library(bslib)
 
 # Hist Demo App modules
-source("modules/mod_home.R")
 source("modules/mod_histogram.R")
 
 # UI app modules
 source("modules/mod_umap.R")
 
 # UI components
+source("ui/home_ui.R")
+source("ui/coming_soon.R")
 source("ui/umap_code.R")
 
 # Set static resource path to www directory
 addResourcePath(prefix = "www", directoryPath = "www")
 
 
-ui <- page_navbar(
-  window_title = "MyInCAT",
+ui <- page_fillable(
+  # theme = ___
 
-  # Head with custom CSS and favicon
-  header = tags$head(
-    tags$link(rel = "icon", type = "image/png", href = "www/logo.png"),
-    tags$link(rel = "stylesheet", type = "text/css", href = "www/custom.css")
-  ),
+  page_navbar(
+    window_title = "MyInCAT",
 
-  # Navbar title as logo link to home
-  title = tags$a(
-    href = "/",
-    tags$img(src = "www/logo.png", height = "30px")
-  ),
+    # Head with custom CSS and favicon
+    header = tags$head(
+      tags$link(rel = "icon", type = "image/png", href = "www/logo.png"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "www/custom.css")
+    ),
 
-  # Home tab
-  nav_panel(
-    title = "Home",
-    mod_home_ui("home")
-  ),
+    # Navbar title as logo link to home
+    title = tags$a(
+      href = "/",
+      tags$img(src = "www/logo.png", height = "30px")
+    ),
 
-  # hist demo tab
-  nav_panel(
-    title = "Hist",
-    mod_histogram_ui("hist")
-  ),
+    # Home tab
+    nav_panel(
+      title = "Home",
+      home_ui()
+    ),
 
-  # UMAP tab
-  nav_panel(
-    title = "UMAP",
-    mod_umap_ui("umap")
-  ),
+    # hist demo tab
+    nav_panel(
+      title = "Hist",
+      mod_histogram_ui("hist")
+    ),
 
-  # DEGs tab
-  nav_panel(
-    title = "DEGs",
-    p("Coming soon...") # TODO
-  ),
+    # UMAP tab
+    nav_panel(
+      title = "UMAP",
+      mod_umap_ui("umap")
+    ),
 
-  # CellChat tab
-  nav_panel(
-    title = "CellChat",
-    p("Coming soon...") # TODO
+    # DEGs tab
+    nav_panel(
+      title = "DEGs",
+      coming_soon() # TODO
+    ),
+
+    # CellChat tab
+    nav_panel(
+      title = "CellChat",
+      coming_soon() # TODO
+    )
   )
 )
 
 server <- function(input, output, session) {
-  mod_home_server("home") # nolint
+  # mod_home_server("home") # nolint
   mod_histogram_server("hist") # nolint
   mod_umap_server("umap") # nolint
 }
