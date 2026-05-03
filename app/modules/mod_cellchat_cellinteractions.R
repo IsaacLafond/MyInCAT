6badace1990829300b37e7516da5da9da454f0bd
Isaac Lafond <ilafo059@uottawa.ca>
2026-05-02 20:17:41 -0400

    [feat] added Cell interactions and heatmap cellchat plots

--- app/modules/mod_cellchat_cellinteractions.R
+++ app/modules/mod_cellchat_cellinteractions.R
@@ -4,8 +4,35 @@
 mod_cellchat_cellinteractions_ui <- function(id) {
   ns <- NS(id)
 
-  page_fillable(
-    coming_soon() # TODO
+  page_fluid(
+    layout_columns(
+      width = c(6, 6),
+      selectInput(
+        ns("source_cells"),
+        label = "Source Cells:",
+        choices = NULL,
+        multiple = TRUE
+      ),
+      selectInput(
+        ns("target_cells"),
+        label = "Target Cells:",
+        choices = NULL,
+        multiple = TRUE
+      )
+    ),
+
+    card(
+      card_header(
+        class = "d-flex justify-content-between align-items-center",
+        "Cell Interaction Table",
+        # downloadButton(ns("download_meta"), "Download CSV", class = "btn-sm")
+      ),
+      dataTableOutput(
+        ns("cell_interactions_table"),
+        height = "450px"
+      ) %>% with_custom_spinner()
+    )
+
   )
 }
 
@@ -14,28 +41,51 @@ mod_cellchat_cellinteractions_ui <- function(id) {
 # -------------------------
 mod_cellchat_cellinteractions_server <- function(id, cellchat_object) {
   moduleServer(id, function(input, output, session) {
-    
+
+    netP <- reactive({
+      x <- subsetCommunication(cellchat_object(), slot.name = "netP")
+      unique(x)
+    })
+
+    observe({
+      req(netP())
+      x <- netP()
+
+      updateSelectInput(
+        session,
+        "source_cells",
+        choices = unique(x$source)
+      )
+      updateSelectInput(
+        session,
+        "target_cells",
+        choices = unique(x$target)
+      )
+    })
+
+    output$cell_interactions_table <- renderDataTable(
+      width = "100%",
+      options = list(
+        paging = FALSE,
+        scrollX = TRUE,
+        scrollY = "300px",
+        scrollCollapse = TRUE
+      ),
+      rownames = FALSE,
+    {
+      x <- netP()
+
+      source_subset <- input$source_cells %||% x$source
+      target_subset <- input$target_cells %||% x$target
+
+      netP_subset <- x[
+        x$source %in% source_subset &
+        x$target %in% target_subset,
+      ]
+
+      netP_subset
+
+    })
+
   })
 }
-
-# THIS PART???
-
-# ## Identify enriched pathways from one cell type (or group of cells) to another
-
-# ```{r}
-# ### Extract communication probabilities at the pathway level
-# netP_subset <- subsetCommunication(#cellchat_orig.ident_interaction.type, slot.name = "netP")
-
-# ### Subset for enriched pathways for target cells to source cells
-# netP_subset <- netP_subset[
-#   netP_subset$source %in% c("#source_cell_1", "#source_cell_2", "#etc.") & 
-#   netP_subset$target %in% c("#target_cell_1", "#target_cell_2", "#etc."), 
-# ]
-
-# ### Get unique pathways from specified cells to target cells
-# pathways_subset <- unique(netP_subset$pathway_name)
-
-
-# head(pathways_subset)
-# ```
-# ### NOTE - have option to download as a csv file or text file!
--- app/modules/mod_cellchat_plots.R
+++ app/modules/mod_cellchat_plots.R
@@ -4,8 +4,69 @@
 mod_cellchat_plots_ui <- function(id) {
   ns <- NS(id)
 
-  page_fillable(
-    coming_soon() # TODO
+  page_fluid(
+    radioGroupButtons(
+        inputId = ns("current_plot"),
+        choices = c("Heatmap", "Circle", "Chord"),
+        selected = "Heatmap",
+        justified = TRUE
+    ),
+
+    conditionalPanel(
+      condition = "input.current_plot == 'Heatmap'",
+      ns = ns,
+      # content:
+      card(
+        card_header(
+          class = "d-flex justify-content-between align-items-center",
+          "Interaction Heatmap",
+          # downloadButton(ns("download_meta"), "Download CSV", class = "btn-sm")
+        ),
+        plotOutput(
+          ns("interaction_heatmap_plot"),
+          width = "100%",
+          height = "66vh"
+        ) %>% with_custom_spinner()
+      ),
+      card(
+        card_header(
+          class = "d-flex justify-content-between align-items-center",
+          "Contribution Heatmap",
+          # downloadButton(ns("download_meta"), "Download CSV", class = "btn-sm")
+        ),
+        card_body(
+          fill = FALSE,
+          selectInput(
+            ns("heatmap_pattern"),
+            label = "Select Pattern:",
+            choices = c(
+              "All" = "all",
+              "Incoming" = "incoming",
+              "Outgoing" = "outgoing"
+            )
+          ),
+          plotOutput(
+            ns("contribution_heatmap_plot"),
+            width = "100%",
+            height = "66vh"
+          ) %>% with_custom_spinner()
+        )
+      )
+    ),
+
+    conditionalPanel(
+      condition = "input.current_plot == 'Circle'",
+      ns = ns,
+      # content:
+      coming_soon() # TODO
+    ),
+
+    conditionalPanel(
+      condition = "input.current_plot == 'Chord'",
+      ns = ns,
+      # content:
+      coming_soon() # TODO
+    )
   )
 }
 
@@ -14,6 +75,31 @@ mod_cellchat_plots_ui <- function(id) {
 # -------------------------
 mod_cellchat_plots_server <- function(id, cellchat_object) {
   moduleServer(id, function(input, output, session) {
-    
+
+    output$interaction_heatmap_plot <- renderPlot({
+      req(cellchat_object)
+
+      netVisual_heatmap(
+        cellchat_object(),
+        slot.name = "net",
+        measure = "weight",
+        color.heatmap = "Reds"
+      )
+    })
+
+    output$contribution_heatmap_plot <- renderPlot({
+      req(cellchat_object, input$heatmap_pattern)
+
+      netAnalysis_signalingRole_heatmap(
+        cellchat_object(),
+        pattern = input$heatmap_pattern,
+        slot.name = "netP",
+        color.heatmap = "Reds",
+        # color.use = 
+        width = 20,
+        height = 20,
+        # font.size = 3
+      )
+    })
   })
 }
